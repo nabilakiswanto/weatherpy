1b5f6e1ccd2b08dc1e8f241306c5a5fd76199182
nabs <nabilakiswanto@gmail.com>
2022-08-16 15:57:38 +0800

    add url api

--- app.py
+++ app.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, request,render_template
+import requests
 
 app = Flask(__name__)
-@app.route("/")
+
+@app.route("/", methods=["GET","POST"])
 def main():
-    return render_template('index.html')
+    weatherData = ''
+    error = 0
+    cityName = ''
+    if request.method == "POST":
+        cityName = request.form.get("cityName")
+        if cityName:
+            weatherApiKey = 'bafa0c89b1cda79638c2bab4713670ce'
+            url = "https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid="+ weatherApiKey
+            weatherData=requests.get(url).json
+        else:
+            error = 1
+
+    return render_template('index.html', data = weatherData, cityName = cityName, error = error)
 if __name__ == "__main__":
     app.run()
