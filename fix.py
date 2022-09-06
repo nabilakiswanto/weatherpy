917fb5b8c26a2e9c1e36ed5d4f0c4a2a56cafab7
nabs <nabilakiswanto@gmail.com>
2022-09-06 12:02:47 +0800

    Update fix.py

--- fix.py
+++ fix.py
@@ -16,7 +16,7 @@ def index():
     cityName = ''
     if request.method == "POST": 
         cityName = request.form.get("cityName")
-        weatherApiKey = 'bafa0c89b1cda79638c2bab4713670ce'
+        weatherApiKey = ''
         url="https://api.openweathermap.org/data/2.5/weather?q="+cityName+"&appid=" + weatherApiKey
         weatherData=requests.get(url).json()
         
